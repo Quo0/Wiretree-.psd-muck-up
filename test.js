f322666b91b309b7593cc563dd60935a9cea0924
Quo0 <intestinesonastick@gmail.com>
2018-02-01 17:54:44 +0400

    added headhesive.js fixed navigation menu. Had to change the markup of HTML page + background layout styles due to conflict to transform and position:fixed propreties. Smooth scroll function now works porperly with new headheasive.js menu

--- test.js
+++ test.js
@@ -10,29 +10,37 @@ function equalHeightBg(){
 
 menuToggler();
 function menuToggler(){
-	var burgerBtn = document.querySelector(".burger");
-	var nav = document.querySelector(".nav");
-	burgerBtn.onclick = function(e){
-		if(burgerBtn.className == "burger"){
-			burgerBtn.className = "burger clicked"
+	var burgerBtn = document.querySelectorAll(".burger");
+	Array.from(burgerBtn).forEach(function(burger){
+		var nav = document.querySelectorAll(".nav");
+		burger.onclick = function(e){
+			if(burger.className == "burger"){
+				burgerBtn[0].className = "burger clicked"
+				burgerBtn[1].className = "burger clicked"
+			}
+			else{
+				burgerBtn[0].className = "burger"
+				burgerBtn[1].className = "burger"
+			}
+			nav.forEach(function(n){
+				if(n.className == "nav"){
+					n.className = "nav clicked"
+				}
+				else{
+					n.className = "nav"
+				}
+				n.onmouseleave = function(){
+					Array.from(nav);
+					nav[0].className = "nav"
+					nav[1].className = "nav"
+				   	burgerBtn[0].className = "burger"
+					burgerBtn[1].className = "burger"
+				}
+			})			
 		}
-		else{
-			burgerBtn.className = "burger"
-		}
-		if(nav.className == "nav"){
-			nav.className = "nav clicked"
-		}
-		else{
-			nav.className = "nav"
-		}
-	}
-	nav.onmouseleave = function(){
-		burgerBtn.className = "burger"
-		nav.className = "nav"
-	}
-	
+	})	
 }
-// slider
+
 window.onload = function slider(){	
 	var sliderWindow = document.querySelector(".sliderWindow");
 	var sliderLine = document.querySelector(".slidesLine");
@@ -80,37 +88,69 @@ window.onload = function slider(){
 	}
 }
 
-
-smoothLinks();
-function smoothLinks(){
-	var nav = document.querySelector("ul.nav");
-	nav.addEventListener("click", function(e){
-		if(e.target.tagName =="A"){
-			e.preventDefault();
-			var linkName = e.target.innerHTML.toLowerCase();
-			var idName = "#"+linkName;
-			var targetBlock = document.querySelector(idName);
-			var y = targetBlock.offsetTop;
-			var scrollPosition = window.pageYOffset;
-			// reseting menu for mobiles
-			var nav = document.querySelector(".nav");
-			var burgerBtn = document.querySelector(".burger");
-			nav.className = "nav";
-			burgerBtn.className = "burger";
-			//
-			scrollToId();
-			function scrollToId(){
-				var timer;
-				if(scrollPosition < y){
-					window.scrollTo(0, scrollPosition);
-					scrollPosition = scrollPosition + 50;
-					timer = setTimeout(scrollToId, 10);
-				}
-				else{
-					clearTimeout(timer);
-					window.scrollTo(0, y); // to the point
+window.onload = function smoothLinks(){
+	var nav = document.querySelectorAll(".nav");
+	var burgerBtn = document.querySelectorAll(".burger");
+	console.log(nav)
+	var headhesiveWrapper = document.querySelector("#Headhesive-wrapper");
+	Array.from(nav).forEach(function(navig){
+		navig.addEventListener("click", function(e){
+			if(e.target.tagName =="A"){
+				console.log(e.target)
+				e.preventDefault();
+				var linkName = e.target.innerHTML.toLowerCase();
+				var idName = "#"+linkName;
+				var targetBlock = document.querySelector(idName);
+				var y = targetBlock.offsetTop - headhesiveWrapper.offsetHeight;
+				var scrollPosition = window.pageYOffset;
+				// reseting menu for mobiles
+				Array.from(nav).forEach(function(n){
+					n.className = "nav";
+				})
+				Array.from(burgerBtn).forEach(function(b){
+					b.className = "burger";
+				})
+				//
+				scrollToId();
+				function scrollToId(){
+					var timer;
+					var step = 50;
+					if(targetBlock.offsetTop < 300){
+						y = targetBlock.offsetTop;
+					}
+					if(scrollPosition < y){
+						window.scrollTo(0, scrollPosition);
+						scrollPosition = scrollPosition + step;
+						if( ((Math.abs(y) - Math.abs(scrollPosition)) >= step) || ((Math.abs(scrollPosition) - Math.abs(y)) >= step)){							
+							timer = setTimeout(scrollToId, 10);
+						}
+							else{
+							clearTimeout(timer);
+							window.scrollTo(0, y); // to the point
+						}
+					}
+					else if(scrollPosition > y){
+						window.scrollTo(0, scrollPosition);
+						scrollPosition = scrollPosition - step;
+						if( ((Math.abs(y) - Math.abs(scrollPosition)) >= step) || ((Math.abs(scrollPosition) - Math.abs(y)) >= step)){							
+							timer = setTimeout(scrollToId, 10);
+						}
+						else{
+							clearTimeout(timer);
+							window.scrollTo(0, y); // to the point
+						}
+					}					
 				}
 			}
-		}
-	})
+		})
+	})	
 }
+
+
+var options = {
+  offset: 500
+}
+var header = new Headhesive('#Headhesive-wrapper', options);
+
+
+
